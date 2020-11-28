6b0b23331468e1a3d93f50d68512050f1e49be27
taiebrekik <taieb@jsproadvisor.com>
2020-11-28 16:29:49 +0100

    Add offer controller

---
+++ app/Http/Controllers/Admin/OffertController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Offert;
+use Illuminate\Http\Request;
+
+class OffertController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Offert  $offert
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Offert $offert)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Offert  $offert
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Offert $offert)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Offert  $offert
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Offert $offert)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Offert  $offert
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Offert $offert)
+    {
+        //
+    }
+}
